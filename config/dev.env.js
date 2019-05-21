aac74e8f563090c7858f7f5f061963abd0e040ec
Francis Lavalliere <Francis.Lavalliere@acentera.com>
2019-05-21 12:24:21 -0400

    added docker

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,9 @@
 const API_HOST = process.env.API_HOST || '127.0.0.1';
+const ENV_CONFIG = process.env.ENV_CONFIG || 'dev';
 
 module.exports = {
   NODE_ENV: '"development"',
-  ENV_CONFIG: '"dev"',
+  ENV_CONFIG: `"${ENV_CONFIG}"`,
   BASE_API: `"http://${API_HOST}:2000/api/"`
 }
 // BASE_API: `"http://${API_HOST}:2000/api/"`
