cf4bd31916c10b40b6aa256a3fd1a36ee5b2b1bb
Francis Lavalliere <Francis.Lavalliere@acentera.com>
2020-04-20 22:12:49 -0400

    fixing entrypoint / dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,39 +2,37 @@ ARG USER
 ARG UID
 ARG GID
 
-FROM acentera/prod:node-serverless-cms-base-v0.0.1
+FROM acentera/prod:acentera-node-python-0.0.3
 
 ARG USER
 ARG UID
 ARG GID
 
-RUN /usr/local/bin/userfix.sh && mkdir -p /usr/node_modules && \
-    chown -R "${USER}:" /usr/node_modules /root
-RUN apk add git
-RUN npm i -g cross-env webpack-dev-server@3.7.1
-RUN npm i cross-env webpack-dev-server@3.7.1
+RUN apk add git make gcc g++
 
-COPY packag*.json /usr/
+# ENV DEBUG=true
+RUN /usr/local/bin/userfix.sh && echo 'ok' && mkdir -p /usr/app /usr/app/node_modules /root/.cache/yarn && \
+    chown -R ${USER}: /root/.npm /root/.config /root/.cache/yarn && \
+    chown ${USER}: /root /usr/app/node_modules /usr && \
+    chown -R ${USER}: /usr/app/node_modules
 
+RUN npm config set prefix "/usr/app/node_modules" && \
+    ln -snf /root/.config /home/${USER}/.config && \
+    chown -R $USER:$(id -gn $USER) /root/.config && \
+    ls -altrh /home/${USER}/
+
+RUN chmod o+rwx /usr /usr/app && chown ${USER}: /usr
+RUN chmod o+rwx /usr/app/node_modules && chown ${USER}: /usr/app/node_modules
 RUN chown ${USER}: /usr/ /usr/app
-RUN su -m -c "cd /usr/; npm i --prefer-offline --no-audit" - "${USER}"
-RUN ls -altrh /usr/node_modules;
 
-# COPY favicon.ico favicon.ico
-# COPY .eslintignore .eslintignore
-# COPY .eslintrc.js .eslintrc.js
-# COPY .babelrc .babelrc
-# COPY .postcssrc.js .postcssrc.js
-# COPY index-template.html index.html
-# 
-# COPY build build
-# COPY config config
-# COPY static static
-# COPY src src
+VOLUME /usr/app/node_modules
+VOLUME /usr/app
+
+ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:.:/usr/local/bin:/bin:/usr/app/node_modules/.bin/:/usr/local/lib/node_modules/npm/bin/:/usr/local/lib/node_modules/npm/bin/node-gyp-bin/
+ENV NODE_PATH=ENV NODE_PATH=.:/usr/app/node_modules:/opt/node_modules/:/opt/:/opt/shared/:/opt/node/node_modules/:/usr/local/lib/node_modules:/usr/local/node_modules/lib/node_modules/:/usr/local/node_modules/:/var/task/
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 RUN chmod +x /usr/local/bin/entrypoint.sh
 
-ENV NODE_PATH=.:/usr/node_modules:/usr/local/node_modules
 WORKDIR /usr/app
 
 EXPOSE 9527
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,52 @@
 #!/bin/sh
-if [ -n "${SITE_TITLE}" ]; then
-  [ -e dist/index.html ] && sed -ri "s/%%TITLE%%/${SITE_TITLE}/g" dist/index.html
-  [ -e index-template.html ] && cat index-template.html | sed -r "s/%%TITLE%%/${SITE_TITLE}/g" > index.html
+set -x
+export PATH="/usr/local/bin/:${PATH}"
+# unset NODE_PATH
+export PATH
+export NODE_PATH
+
+chmod o+rwx /usr /usr/app
+chown ${USER}: /usr /usr/app
+cd /usr/app/
+[ -e package-lock.json ] && rm package*lock*
+
+if [ -e /usr/app/node_modules/ ]; then
+  cp -Rf /usr/local/lib/node_modules/fibers /usr/app/node_modules/
 fi
-chmod o+rw index.html
-[ -n "${USER}" ] && chown ${USER} index.html
 
-[ ! -e index.html ] && cat index-template.html > index.html
-
-# chmod o+rwx node_modules/
-# 
-# set -x
-# [ ! -e 'node_modules/node-module-done' ] && (npm i && touch 'node_modules/node-module-done')
-[ -e node_modules ] && rm -fr node_modules
-ln -snf ../node_modules
-# su -m -c '
-# npm i
-# ' - ${USER}
-
-# [ ! -e 'node_modules/node-sass/vendor/linux_musl-x64-72/binding.node' ] && npm rebuild node-sass
-# #npm rebuild node-sass
-
-export NODE_PATH=${NODE_PATH}:.:/usr/node_modules/:/usr/local/node_modules/
+find /usr/app/node_modules -not -user ${USER} -exec chown ${USER} {} \;
 
 cat > /tmp/.run <<EOF
-cd /usr/;
-npm ci --prefer-offline --no-audit;
+mkdir -p /usr/app/node_modules
+cd /usr/app/;
+ls -atlrh
+whoami
+export PATH="/usr/local/bin/:${PATH}"
+export PATH="${PATH}"
+export NODE_PATH="${NODE_PATH}"
+export NODE_ENV="${NODE_ENV}"
+npm config set prefix "/usr/app/node_modules"
+npm i --no-audit 2>/dev/null;
+node /usr/local/proxy.js &
+(cd /usr/app/node_modules/fibers/; node-gyp configure; /usr/local/bin/node /usr/app/node_modules/fibers/build)
 cd app;
+(cd /usr/app; watchexec -f '*package*.json' --force-poll 5000 -i vue-ssr-client-manifest.json,babel.config.json,nodemon.json,package-lock.json --verbose -s SIGKILL -r 'echo Relaunching NPM i; cd /usr/; [ -e package-lock.json ] && rm -f package-lock.json; npm i --prefer-offline --no-audit') &
+if [ ! -e dist/ ]; then
+  npm run build-all
+fi;
 $@
 EOF
+#yarn run build
+# yarn run dev
+# $@
 chmod +x /tmp/.run
+npm config set prefix "/usr/app/node_modules"
 
-# rm -fr node_modules;
-# cp -Rfp /usr/node_modules .
+cd /usr/app;
+[ -e node_modules/.cache/babel-loader ] && rm -rf node_modules/.cache/babel-loader
 
-echo  /bin/sh -m -c \"cd /usr/app; $@\" - \"${USER}\"
-exec /bin/sh -m -c "cd /usr/app; ls -latrh; /tmp/.run" - ${USER}
+echo "WILL CREATE SYMLINK ln -snf /usr/app/postcss.config.js /usr/postcss.config.js"
+export PATH="${PATH}:/usr/app/node_modules/.bin"
+export NODE_ENV
+
+exec /tmp/.run
